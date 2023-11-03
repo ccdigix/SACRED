1fde68456dd71ddd53989b7335550db2977f8a22
ccdigix <clohosy13@gmail.com>
2023-11-03 14:34:37 +0000

    Wrap SACRED within the Pode Powershell server.

--- src/SACRED.Server/SACRED.Server.Pode/SACRED.Server.Pode.psm1
+++ src/SACRED.Server/SACRED.Server.Pode/SACRED.Server.Pode.psm1
@@ -40,6 +40,58 @@ Function Initialize-SACREDPodeServerEnvironment (
     Initialize-SACREDEnvironment -StoreType (Get-PodeConfig).SACRED.StoreType -LocalStoreBasePath (Get-PodeConfig).SACRED.LocalStoreBasePath -LoggerType (Get-PodeConfig).SACRED.LoggerType -LocalLoggerBasePath (Get-PodeConfig).SACRED.LocalLoggerBasePath -SecretStoreType (Get-PodeConfig).SACRED.SecretStoreType -ConnectToAzure:((Get-PodeConfig).SACRED.ConnectToAzure) -AzureTenantId (Get-PodeConfig).SACRED.AzureTenantId -UseAzureManagedIdentity:((Get-PodeConfig).SACRED.UseAzureManagedIdentity) -AzureServicePrincipalClientId (Get-PodeConfig).SACRED.AzureServicePrincipalClientId -AzureServicePrincipalClientSecret (Get-PodeConfig).SACRED.AzureServicePrincipalClientSecret -AzureServicePrincipalClientCertificateThumbprint (Get-PodeConfig).SACRED.AzureServicePrincipalClientCertificateThumbprint
 }
 
+Function ConvertTo-Base64ValueBytes (
+    [Parameter(Mandatory=$true)]    
+    [string] $Value
+) 
+{
+    $Value = ($Value -ireplace '-', '+')
+    $Value = ($Value -ireplace '_', '/')
+
+    switch ($Value.Length % 4) 
+    {
+        1 {
+            #$Value = $Value.Substring(0, $Value.Length - 1)
+            $Value += '==='
+        }
+
+        2 {
+            $Value += '=='
+        }
+
+        3 {
+            $Value += '='
+        }
+    }
+
+    try 
+    {
+        $base64Bytes = [System.Convert]::FromBase64String($Value)
+        return $base64Bytes
+    }
+    catch 
+    {
+        throw 'Invalid Base64 encoded value.'
+    }
+}
+
+Function ConvertFrom-Base64ValueString (
+    [Parameter(Mandatory=$true)]    
+    [string] $Value
+)
+{
+    try 
+    {
+        $base64Bytes = ConvertTo-Base64ValueBytes -Value $Value
+        $Value = [System.Text.Encoding]::UTF8.GetString($base64Bytes)
+        return $Value
+    }
+    catch 
+    {
+        throw 'Invalid Base64 encoded value.'
+    }
+}
+
 Function Start-SACREDPodeServer (
     [Parameter(Mandatory=$false)]
     [int] $ServerThreads = 3
@@ -99,34 +151,92 @@ Function Start-SACREDPodeServer (
         }
 
         New-PodeAccessScheme -Type Role | Add-PodeAccess -Name 'RoleAccess'
-        New-PodeAuthScheme -ApiKey | Add-PodeAuth -Name 'Authenticate' -Sessionless -ScriptBlock {
-            param($key)
 
-            $roles = @()
+        if((Get-PodeConfig).SACRED.ApiAuthentication -eq 'ApiKey')
+        {
+            New-PodeAuthScheme -ApiKey | Add-PodeAuth -Name 'Authenticate' -Sessionless -ScriptBlock {
+                param($key)
 
-            Initialize-SACREDPodeServerEnvironment
-            
-            $rotationJobAuthorApiKey1 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobAuthorApiKey1SecretName)
-            $rotationJobAuthorApiKey2 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobAuthorApiKey2SecretName)
-            if(($rotationJobAuthorApiKey1 -eq $key) -or ($rotationJobAuthorApiKey2 -eq $key))
-            {
-                $roles += 'RotationJobAuthor'
-            }
+                $roles = @()
 
-            $rotationJobExecutorApiKey1 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobExecutorApiKey1SecretName)
-            $rotationJobExecutorApiKey2 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobExecutorApiKey2SecretName)
-            if(($rotationJobExecutorApiKey1 -eq $key) -or ($rotationJobExecutorApiKey2 -eq $key))
-            {
-                $roles += 'RotationJobExecutor'
-            }
-
-            $user = @{
-                User = @{
-                    Username = 'APIKeyUser'
-                    Roles = $roles
+                Initialize-SACREDPodeServerEnvironment
+                
+                $rotationJobAuthorApiKey1 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobAuthorApiKey1SecretName)
+                $rotationJobAuthorApiKey2 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobAuthorApiKey2SecretName)
+                if(($rotationJobAuthorApiKey1 -eq $key) -or ($rotationJobAuthorApiKey2 -eq $key))
+                {
+                    $roles += 'RotationJobAuthor'
                 }
+
+                $rotationJobExecutorApiKey1 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobExecutorApiKey1SecretName)
+                $rotationJobExecutorApiKey2 = $global:SACREDSecretStore.GetSecret((Get-PodeConfig).SACRED.RotationJobExecutorApiKey2SecretName)
+                if(($rotationJobExecutorApiKey1 -eq $key) -or ($rotationJobExecutorApiKey2 -eq $key))
+                {
+                    $roles += 'RotationJobExecutor'
+                }
+
+                $user = @{
+                    User = @{
+                        Username = 'APIKeyUser'
+                        Roles = $roles
+                    }
+                }
+                return $user
             }
-            return $user
+        }
+        elseif((Get-PodeConfig).SACRED.ApiAuthentication -eq 'EntraServicePrincipalJWT')
+        {
+            New-PodeAuthScheme -Bearer | Add-PodeAuth -Name 'Authenticate' -Sessionless -ScriptBlock {
+                param($token)
+    
+                $tokenParts = $token.Split('.')
+                $header = (ConvertFrom-Base64ValueString -Value $tokenParts[0] | ConvertFrom-Json)
+                $body = (ConvertFrom-Base64ValueString -Value $tokenParts[1] | ConvertFrom-Json)
+    
+                $openIdConfig = Invoke-RestMethod -Method Get -Uri 'https://login.microsoftonline.com/common/.well-known/openid-configuration'
+                $jwksConfig = Invoke-RestMethod -Method Get -Uri $openIdConfig.jwks_uri
+                $kid = $header.kid
+                $signingKey = $jwksConfig.keys | Where-Object {$_.kid -eq $kid}
+    
+                $cryptoServiceProvider = New-Object System.Security.Cryptography.RSACryptoServiceProvider
+                $cryptoServiceProvider.ImportParameters(
+                    @{
+                        Modulus = (ConvertTo-Base64ValueBytes -Value $signingKey.n)
+                        Exponent = (ConvertTo-Base64ValueBytes -Value $signingKey.e)
+                    }
+                )
+                $sha256 = [System.Security.Cryptography.SHA256]::Create()
+                $hash = $sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($tokenParts[0] + '.' + $tokenParts[1]))
+                $rsaDeformatter = New-Object System.Security.Cryptography.RSAPKCS1SignatureDeformatter($cryptoServiceProvider)
+                $rsaDeformatter.SetHashAlgorithm('SHA256')
+                $validSignature = $rsaDeformatter.VerifySignature($hash, (ConvertTo-Base64ValueBytes($tokenParts[2])))
+    
+                if(!$validSignature)
+                {
+                    return $null
+                }
+    
+                $clientId = (Get-PodeConfig).SACRED.ClientId
+                if($body.aud -ne $clientId)
+                {
+                    return $null
+                }
+    
+                $now = [datetime]::UtcNow
+                $unixStart = [datetime]::new(1970, 1, 1, 0, 0, [DateTimeKind]::Utc)
+                if(($now -gt $unixStart.AddSeconds($body.exp)) -or ($now -lt $unixStart.AddSeconds($body.nbf)))
+                {
+                    return $null
+                }
+    
+                $user = @{
+                    User = @{
+                        Username = $body.appid
+                        Roles = $body.roles
+                    }
+                }
+                return $user
+            }
         }
 
         Add-PodeRouteGroup -Path '/api' -Authentication 'Authenticate' -Role 'RotationJobAuthor' -Access 'RoleAccess' -Routes {
